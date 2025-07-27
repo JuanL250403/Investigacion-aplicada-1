68afa8af7e5b9f663cdc173bc3fde45663e6d1a6
LM250403 <juaeivamont@gmail.com>
2025-07-27 13:49:04 -0600

    Añadiendo una WEBAPP

---
+++ WEB_APP/js/script.js
@@ -0,0 +1,56 @@
+let ant = document.getElementById("anterior");
+let sig = document.getElementById("siguiente");
+
+let slides = document.getElementsByClassName("slide");
+
+var a = 0;
+var s = 0;
+
+sig.addEventListener("click", function(){
+    if (a < slides.length - 1){
+        a++
+        slides[a].style.order = "-1"
+        slides[a-1].removeAttribute("style");
+    }
+    console.log("a" + a)
+})
+ant.addEventListener("click", function(){
+    s = a;
+    if (s > 0){
+        a--
+        slides[s-1].style.order = "-1"
+        slides[s].removeAttribute("style");
+    }
+    console.log("s" + s)
+})
+
+var ida = 0;
+var movil = false;
+window.addEventListener("resize", function(){
+    if (window.innerWidth < 460){
+        movil = true;
+    }
+    else{
+        movil = false;
+    }
+}, false);
+function loop(movil){
+    console.log("j")
+    if (movil == true){
+        setTimeout(function(){
+            console.log("hola")
+            ida++
+            if (ida >= slides.length){
+                ida = 0;
+            }
+            if (ida < slides.length){
+                slides[ida].style.order = "-1"
+                if (ida > 0){
+                    slides[ida-1].removeAttribute("style");
+                }
+            }
+            console.log(ida)
+            loop();
+        }, 6000) 
+    }
+    }
